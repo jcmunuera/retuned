691cdbf810457c9379efce3f7e427b8322f57f51
jcmunuera <jcmunuera@gmail.com>
2025-04-03 16:00:57 +0200

    Rename sections with a name accordingly with its content

--- src/components/FanClub.tsx
+++ src/components/FanClub.tsx
@@ -1,7 +1,7 @@
 // src/components/FanClub.tsx
 export default function FanClub() {
   return (
-    <section id="section1" className="min-h-screen bg-purple-500 flex items-center justify-center">
+    <section id="section1" className="min-h-screen bg-green-500 flex items-center justify-center">
       <h2 className="text-3xl text-white">Hello World - Fan Club</h2>
     </section>
   );
